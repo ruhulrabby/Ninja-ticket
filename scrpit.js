3e440b1e9088b6c66724afd5b25dc62cfeb3ec07
ruhul rabby <ruhulalam1994@gmail.com>
2021-01-30 02:26:54 +0600

    icremnt and decremnt button done

--- scrpit.js
+++ scrpit.js
@@ -1,23 +1,37 @@
 // First class increment button event listener
 document.getElementById('first-class-increment-btn').addEventListener("click", function() {
-    console.log("someone clicked 1st class + btn");
+    const firstClassInput = document.getElementById("first-class-input");
+    const firstClassCount = parseInt(firstClassInput.value);
+    const firstClassNewCount = firstClassCount + 1;
+    firstClassInput.value = firstClassNewCount;
+    
 });
-
 
 // First class Decrement button event handler
 document.getElementById('first-class-decrement-btn').addEventListener("click", function(){
-    console.log("someone clicked 1st class - button");
+    const firstClassInput = document.getElementById("first-class-input");
+    const firstClassCount = parseInt(firstClassInput.value);
+    const firstClassNewCount = firstClassCount - 1;
+    firstClassInput.value = firstClassNewCount;
 });
 
 
 // Economy class Increment button event listener
 document.getElementById("economy-class-increment-btn").addEventListener("click", function(){
-    console.log("someone clicked e class + btn");
+    const economyClassInput = document.getElementById("economy-class-input");
+    const economyClassCount = parseInt(economyClassInput.value);
+    const economyClassNewCount = economyClassCount + 1;
+    economyClassInput.value = economyClassNewCount;
 } );
 
 
 // Economy class Decrement button event listener
 document.getElementById("economy-class-decrement-btn").addEventListener("click", function(){
-    console.log("someone clicked e class - btn");
+    const economyClassInput = document.getElementById("economy-class-input");
+    const economyClassCount = parseInt(economyClassInput.value);
+    const economyClassNewCount = economyClassCount - 1;
+    economyClassInput.value = economyClassNewCount;
 } );
+
+
 
