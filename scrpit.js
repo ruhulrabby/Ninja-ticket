5e44d6dad55ee858a5c8cfd98f38e71d3a7410cd
ruhul rabby <ruhulalam1994@gmail.com>
2021-01-30 01:38:28 +0600

    added event listener to all the buttons and rest input value to 0

---
+++ scrpit.js
@@ -0,0 +1,23 @@
+// First class increment button event listener
+document.getElementById('first-class-increment-btn').addEventListener("click", function() {
+    console.log("someone clicked 1st class + btn");
+});
+
+
+// First class Decrement button event handler
+document.getElementById('first-class-decrement-btn').addEventListener("click", function(){
+    console.log("someone clicked 1st class - button");
+});
+
+
+// Economy class Increment button event listener
+document.getElementById("economy-class-increment-btn").addEventListener("click", function(){
+    console.log("someone clicked e class + btn");
+} );
+
+
+// Economy class Decrement button event listener
+document.getElementById("economy-class-decrement-btn").addEventListener("click", function(){
+    console.log("someone clicked e class - btn");
+} );
+
